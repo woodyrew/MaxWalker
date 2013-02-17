db7d17519e14e902b5a56d613d5dddb3fc88a6dc
Woody Goodricke <github@andrewgoodricke.com>
2013-02-17 17:49:51 +0000

    AngularJS seed from Yeoman With Twitter Bootstrap, Bower and Grunt.js

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('MaxWalkerApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
